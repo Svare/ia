93dd6a122ebf716c73f9d084111331b8369cb5c6
Servando Miguel <servandomiguel@localhost.localdomain>
2018-03-09 21:14:29 -0600

    Modificaciones a agente

--- garbageAgents.py
+++ garbageAgents.py
@@ -7,97 +7,26 @@
 
 from random import randint
 
-import os
-import time
-
-def create_enviroment(rows,columns):
-	
-	''' Crea una lista de listas inicializadas en ceros
-		la cual va a simular el ambiente de los agentes
-
-		Parámetros:
-
-		rows -> entero, número de filas del ambiente
-		columns -> entero, número de columnas del ambiente
-
-		Retorna: lista '''
-
-	enviroment = ['0']*rows
-	for row in range(rows):
-		enviroment[row] = ['0']*columns
-	return enviroment
-
-def litter(fila,how_much_garbage):
-
-	''' Inserta basura en posiciones aleatorias de una
-		fila
-
-		Parámetros:
-
-		fila -> lista, fila a la que le voy a insertar basura
-		how_much_garbage -> entero, cuanta basura quiero
-							que genere
-
-		Retorna: None '''
-
-	for i in range(how_much_garbage):
-		fila[randint(0,len(fila)-1)] = 'W'
-
-def litter_the_enviroment(enviroment):
-
-	''' Coloca basura en todo el ambiente
-
-		Parámetros:
-
-		enviroment -> lista, es una lista de listas que 
-						representa el ambiente de los
-						agentes
-
-		Retorna: lista '''
-
-	for row in enviroment:
-		litter(row,randint(0,5))
-	return enviroment
-
-def print_enviroment(enviroment):
-
-	''' Imprime en la terminal el ambiente
-
-		Parámetros:
-
-		enviroment -> lista, es una lista de listas que 
-						representa el ambiente de los
-						agentes
-
-		Retorna: None '''
-
-	for row in enviroment:
-		print(row)
-
 class Agent:
 
-	# Atributos
+	directions = ('N','S','E','O')
+	
 
-	x_pos = 0
-	y_pos = 0
+	def __init__(self, symbol='*',name='Robotina', x_pos=0, y_pos=0, orientation=0):
+		self.symbol = symbol 
+		self.name = name
+		self.x_pos = x_pos
+		self.y_pos = y_pos
+		orientation = 0 if orientation > 3 else orientation
+		self.orientation = directions[orientation]
 
-def main():
-	rows = int(input('Filas: '))
-	columns = int(input('Columnas: '))
-	os.system('clear')
 
-	enviroment = litter_the_enviroment(create_enviroment(rows,columns))
-	#print_enviroment(enviroment)
+	def collision(self, environment):
+		pass
 
-	enviroment[0][0] = '*'
-	print_enviroment(enviroment)
-	time.sleep(3)
-	os.system('clear')
-	enviroment[0][0] = '0'
-	enviroment[0][1] = '*'
-	print_enviroment(enviroment)
-	time.sleep(5)
-	os.system('clear')
+	def take_garbage(self, )
+
+
 
 
 
